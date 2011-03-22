60898c69e26ec688b2b33350f67b9bd40227c38b
Matthew Wastrodowski <towski@gmail.com>
2011-03-21 21:12:47 -0700

    plants only grow when the sun is out

--- Assets/scripts/plant.js
+++ Assets/scripts/plant.js
@@ -2,12 +2,12 @@ private var soil;
 private var sun;
 
 function initialize(mySoil, mySun){
-  sun = mySun;
+  sun = mySun.GetComponent("sun");
   soil = mySoil.GetComponent("soil");
 }
 
 function grow(){
-  if(soil.watered()){
+  if(soil.watered() && sun.day){
     if(soil.hasNutrients()){
       transform.localScale.y += 0.01;
       soil.drainNutrients();
--- Assets/scripts/sun.js
+++ Assets/scripts/sun.js
@@ -1,13 +1,18 @@
+var day;
+
 function Start(){
+  day = true;
   StartCoroutine(cycle());
 }
 
 function cycle(){
   while(true){
     yield WaitForSeconds(10.0);
-    if(light.intensity == 0.1){
+    if(day == false){
+      day = true;
       light.intensity = 0.3;
     }else{
+      day = false;
       light.intensity = 0.1;
     }
   }
